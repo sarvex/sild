51d752cc56b7364568a42a96e77a6108b8a31b7c
Jeff Fowler <jeffowler@gmail.com>
2016-02-29 20:42:55 -0500

    create a second cell, point at it from the first cell, print everything out

--- sild.c
+++ sild.c
@@ -6,10 +6,16 @@ struct Cell {
 };
 
 int main() {
+    struct Cell another_cell;
+    another_cell.value = 2;
+    another_cell.next = 0x0;
+
     struct Cell a_cell;
     a_cell.value = 1;
-    a_cell.next = 0x0;
+    a_cell.next = &another_cell;
 
     printf("a_cell's value is: %d\n", a_cell.value);
-    printf("the next cell after a_cell is: %p", a_cell.next);
+    printf("the next cell after a_cell is: %p, which is called another_cell\n", a_cell.next);
+    printf("another_cell's value is: %d\n", another_cell.value);
+    printf("the next cell after another_cell is: %p", another_cell.next);
 }
