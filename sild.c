6b8a7f9f7675738e7dcc5335986cde14d867a14a
Jeff Fowler <jeffowler@gmail.com>
2016-02-29 20:42:12 -0500

    create and assign a Cells values, print them out

--- sild.c
+++ sild.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
 struct Cell {
     int value;
     struct Cell * next;
 };
+
+int main() {
+    struct Cell a_cell;
+    a_cell.value = 1;
+    a_cell.next = 0x0;
+
+    printf("a_cell's value is: %d\n", a_cell.value);
+    printf("the next cell after a_cell is: %p", a_cell.next);
+}
