2a4774cadc9bf6a276db9955a17ba3be44b71678
urthbound <jeffowler@gmail.com>
2016-03-02 00:36:44 -0500

    with pointers, everything works, also arrow notation

--- sild.c
+++ sild.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Cell {
     int value;
     struct Cell * next;
 };
 
-void print_list(struct Cell car) {
-    printf("%d ", car.value);
-    if (car.next) {
-        print_list(*car.next);
+void print_list(struct Cell *car) {
+    printf("%d ", car->value);
+    if (car->next) {
+        print_list(car->next);
     }
 }
 
-struct Cell makecell(int value, struct Cell *next) {
-    struct Cell outcell = { value, next };
+struct Cell *makecell(int value, struct Cell *next) {
+    struct Cell *outcell = malloc(sizeof(struct Cell));
+    outcell->value = value;
+    outcell->next = next;
     return outcell;
 };
 
 
 int main() {
-    struct Cell a_cell = makecell(1, &makecell(2, 0x0));
+    struct Cell *a_cell = makecell(1, makecell(2, 0x0));
     print_list(a_cell);
 }
