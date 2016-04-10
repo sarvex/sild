9467084203f74aee764f01eeedb042c243bfc41c
Jeff Fowler <jeffowler@gmail.com>
2016-04-10 14:07:04 -0400

    adding debug list comment out

--- sild.c
+++ sild.c
@@ -240,5 +240,6 @@ int main() {
     C *a_list          = read(&a_string);
     C *an_evalled_list = eval(a_list);
                          print(an_evalled_list);
+                         /* debug_list(an_evalled_list); */
     return 0;
 }
