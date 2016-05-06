a3bce33c45c2191c25a4e0e1da1b9c9166a6b2db
Jeff Fowler <jeffowler@gmail.com>
2016-05-06 18:22:52 -0400

    entry vs env

--- src/env.c
+++ src/env.c
@@ -4,22 +4,34 @@
 #include "cell.h"
 #include "util.h"
 
-struct Env {
+typedef struct Entry {
     char *key;
     C *value;
+} Entry;
+
+struct Env {
+    struct Entry *head;
 };
 
 C *get(Env* env, C *key) {
-    if (scmp(key->val.label, env->key)) {
+    if (scmp(key->val.label, env->head->key)) {
         return truth();
     } else {
         return empty_list();
     }
 }
 
-Env * new_env() {
-    Env *out = malloc(sizeof(Env));
+static Entry *new_entry() {
+    Entry *out = malloc(sizeof(Entry));
+    if (!out) { exit(1); };
     out->key = "derp";
     out->value = NULL;
+    return out;
+}
+
+Env *new_env() {
+    Env *out = malloc(sizeof(Env));
+    if (!out) { exit(1); };
+    out->head = new_entry();
     return out;
 }
