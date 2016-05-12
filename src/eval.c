29e3cb2055059fda1ca31af3857a18f512c40aeb
urthbound <jeffowler@gmail.com>
2016-05-12 14:09:22 -0400

    newline on proc arity err

--- src/eval.c
+++ src/eval.c
@@ -26,8 +26,10 @@ static C *apply_proc(C* proc, Env *env) {
     int arity = count_list(cur);
     int numpassed = count_list(curarg);
 
+    printf("%i %i\n", arity, numpassed);
+
     if (arity != numpassed) {
-        printf("arity error on proc application");
+        printf("arity error on proc application\n");
         exit(1);
     }
 
